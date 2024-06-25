2242a4f245bf051f2203161b5f115435b3df48b1
R-kjim <rkayjim@gmail.com>
2024-06-25 17:27:00 +0300

    mathematical operation in js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1=31
+let num2=2
+let multiply=num1*num2
+let random=Math.round(Math.random())+1
+let num3=24
+let num4=5
+let mod=num3%num4
+let max=Math.max(10,14,20,17,5)
